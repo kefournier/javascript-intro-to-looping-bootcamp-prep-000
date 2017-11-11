84022a33df35ae7e401dbbb3521778ce3f8736fb
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-11-11 14:08:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,10 +22,10 @@ function doWhileLoop(array) {
   function maybeTrue() {
      return Math.random() >= 0.5
    }
- 
+
    do {
      array = array.pop()
    } while (array.length > 0 && maybeTrue())
- 
+
    return array
  }
