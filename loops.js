8b63822468414f478cd349da05b04c0e742b2d76
Kirsten Fournier <kfournier@hopkinton.k12.ma.us>
2017-11-11 14:10:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -18,14 +18,16 @@ function whileLoop(n) {
   return("done");
 }
 
-function doWhileLoop(array) {
-  function maybeTrue() {
-     return Math.random() >= 0.5
-   }
-
-   do {
-     array = array.pop()
-   } while (array.length > 0 && maybeTrue())
-
+function maybeTrue() {
+   return Math.random() >= 0.5
+  }
+ 
+  function doWhileLoop(array){
+     do {
+    array.shift()
+        }
+   while (array.length > 0 && maybeTrue());
+    //  && means both condition must be true. 
+ 
    return array
  }
